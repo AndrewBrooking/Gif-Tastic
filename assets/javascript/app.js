00d62aaa7044d42b2de1bd5065d5e34eecfddc76
Andrew Brooking <atbrooking.dev@gmail.com>
2019-07-26 12:43:11 -0500

    Added Masonry.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,107 +11,139 @@ let queries = [
     "robots"
 ];
 
-$(window).on('load', function() {
+$(document).ready(function () {
+
     // Initialize Masonry
-    $('div.grid').masonry({
-        itemSelector: 'div.grid-item',
-        columnWidth: 'div.grid-item',
+    let grid = $(".grid").masonry({
+        itemSelector: ".grid-item",
+        columnWidth: ".grid-sizer",
+        fitWidth: true,
         percentPosition: true
     });
-});
 
-$(document).ready(function() {
     // Show default buttons when page has loaded
     displayButtons();
 
+    // Handle button click events
     $(document).on("click", "button", buttonClick);
-});
 
-// Create a query URL with a search term
-function generateQueryURL(term) {
-    return url + "api_key=" + api_key + "&q=" + term;
-}
+    // Handle image hover events
+    $(document).on("hover", "img", imgHover);
 
-// Creates a new button and adds it to the button group
-function addButton(value) {
-    let btn = $("<button>")
-        .addClass("btn btn-primary w-100 text-capitalize align-top")
-        .val(value)
-        .text(value);
-
-    $("#btn-group").append(btn);
-}
-
-// Renders buttons in the button group
-function displayButtons() {
-    $("#btn-group").empty();
-
-    for (let q of queries) {
-        addButton(q);
+    // Create a query URL with a search term
+    function generateQueryURL(term) {
+        return url + "api_key=" + api_key + "&q=" + term;
     }
-}
 
-// Hanldes button clicks
-function buttonClick(event) {
-    // Stop page from refreshing
-    event.preventDefault();
+    // Creates a new button and adds it to the button group
+    function addButton(value) {
+        let btn = $("<button>")
+            .addClass("btn btn-primary w-100 text-capitalize align-top")
+            .val(value)
+            .text(value);
 
-    // Obtain value of button that was clicked
-    let value = $(this).val();
+        $("#btn-group").append(btn);
+    }
 
-    // Check if a search was performed
-    if (value === "search-btn") {
-        // Get value for new query
-        let newV = $("#search").val();
-        newV = newV.toLowerCase().trim();
+    // Renders buttons in the button group
+    function displayButtons() {
+        $("#btn-group").empty();
 
-        // Empty search field
-        $("#search").val("");
+        for (let q of queries) {
+            addButton(q);
+        }
+    }
 
-        // Check that the new value is not empty
-        if (newV === "") {
-            return;
+    // Hanldes button clicks
+    function buttonClick(event) {
+        // Stop page from refreshing
+        event.preventDefault();
+
+        // Obtain value of button that was clicked
+        let value = $(this).val();
+
+        // Check if a search was performed
+        if (value === "search-btn") {
+            // Get value for new query
+            let newV = $("#search").val();
+            newV = newV.toLowerCase().trim();
+
+            // Empty search field
+            $("#search").val("");
+
+            // Check that the new value is not empty
+            if (newV === "") {
+                return;
+            }
+
+            // Add the new value to the queries array
+            queries.push(newV);
+
+            // Display new button
+            displayButtons();
+
+            // Set value to the new value to run an ajax query
+            value = newV;
         }
 
-        // Add the new value to the queries array
-        queries.push(newV);
+        // Create a new query URL
+        let queryURL = generateQueryURL(value);
 
-        // Display new button
-        displayButtons();
-
-        // Set value to the new value to run an ajax query
-        value = newV;
+        // Run an ajax query
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(populateImages);
     }
 
-    // Create a new query URL
-    let queryURL = generateQueryURL(value);
+    // Displays first 25 gifs from the giphy response
+    function populateImages(response) {
+        console.log(response);
 
-    // Run an ajax query
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    }).then(populateImages);
-}
+        // Remove the previous results
+        grid.masonry().empty();
 
-// Displays first 25 gifs from the giphy response
-function populateImages(response) {
-    console.log(response);
+        // Adds a masonry sizing div
+        let sizer = $("<div>").addClass(".grid-sizer");
+        appendMasonryItem(sizer);
 
-    // Remove the previous results
-    $("div.grid").empty();
-
-    // Add all images to the page
-    for (let i = 0; i < response.data.length; i++) {
-        newImage(response.data[i].images.downsized.url);
+        // Add all images to the page
+        for (let i = 0; i < response.data.length; i++) {
+            let data = response.data[i];
+            newImage(
+                data.title,
+                data.images.downsized_still.url,
+                data.images.downsized.url
+            );
+        }
     }
-}
 
-// Creates a new img element
-function newImage(src) {
-    let item = $("<div>").addClass("grid-item");
-    item.append(
-        $("<img>").attr("src", src)
-    );
+    // Add a new gif to the page
+    function newImage(alt, still, animated) {
+        // Create div to store img
+        let item = $("<div>").addClass("grid-item");
 
-    $("div.grid").append(item);
-}
+        // Create img with still and animated data values
+        let img = $("<img>")
+            .attr("src", still)
+            .attr("alt", alt)
+            .attr("data-still", still)
+            .attr("data-animated", animated)
+            .attr("data-status", "still");
+
+        // Add item to the results div
+        item.append(img);
+        appendMasonryItem(item);
+    }
+
+    function imgHover() {
+        // TODO
+    }
+
+    function appendMasonryItem(item) {
+        grid.masonry()
+            .append(item)
+            .masonry("appended", item)
+            .masonry("layout");
+    }
+});
